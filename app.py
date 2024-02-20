2e5777ee8ed48e42a486fd330ae44cab5fec66c8
fulcinm <155673607+fulcinm@users.noreply.github.com>
2024-02-19 22:23:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,2 @@
-import streamlit as st
-st.header('Tossing a Coin')
-st.write('It is not a functional application yet. Under construction.')
-
-.git .gitignore .streamlit app.py README.md requirements.txt
-
-$ git commit -am "initial commit"
-$ git push 
-
 pip install streamlit 
 streamlit run app.py 
